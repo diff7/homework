f885ce051b42b29d1f77e2af9b3b8df80c88da40
Egor <geksut@gmail.com>
2020-01-05 20:39:09 +0300

    random forest

--- trees/tree.py
+++ trees/tree.py
@@ -57,12 +57,14 @@ class Node:
                 entropy, 
                 num_samples, 
                 num_samples_per_class, 
-                predicted_class):
+                predicted_class,
+                predicted_proba):
    
         self.entropy = entropy
         self.num_samples = num_samples
         self.num_samples_per_class = num_samples_per_class
-        self.predicted_class = predicted_class
+        self.predicted_proba = predicted_proba
+        self.predicted_class = predicted_class 
         self.feature = 0
         self.threshold = 0
         self.left = None
@@ -85,10 +87,12 @@ class DecisionTree(Stump):
         ent = 0
         ent = -sum((n/m)*np.log(n/m) for n in count_by_class if n!=0)
         predicted_class = np.argmax(count_by_class)
+        predicted_proba = np.array(count_by_class)/m
         node = Node( ent, 
                     len(targets),
                     count_by_class,
-                    predicted_class)
+                    predicted_class,
+                    predicted_proba)
             
         if depth < self.max_depth:
             value, feature = self.get_split(data, targets)
@@ -115,3 +119,65 @@ class DecisionTree(Stump):
             else:
                 node = node.left
         return node.predicted_class
+    
+    def predict_proba(self, data):
+        return np.array([self._predict_single_proba(row) for row in data])
+    
+    def _predict_single_proba(self, values):
+        node = self.tree
+        
+        while node.left:
+            if values[node.feature] > node.threshold:
+                node = node.right
+            else:
+                node = node.left
+        return node.predicted_proba
+    
+    
+    
+class RandomForest:
+    def __init__(self, 
+                 max_features = 0.7,
+                 min_samples = 0.2,
+                 max_depth = 6, # inclusive
+                 num_trees=1,
+                 min_depth = 4):   
+        
+        self.max_features = max_features
+        self.min_samples = min_samples
+        self.max_depth = max_depth
+        self.num_trees = num_trees
+        self.min_depth = min_depth
+    
+    def plant_forest(self):
+        self.forest =[]
+        for i in range(self.num_trees):
+            depth =  np.random.randint(self.min_depth,self.max_depth+1)
+            tree = DecisionTree(maxdepth=depth)
+            self.forest.append(tree)
+        return self.forest
+    
+    def random_slice(self, data, targets):
+        # random choice from  min sample size - max sample size 
+        num_rows = np.random.randint(int(len(targets)*self.min_samples), targets.shape)
+        row_index = np.random.choice(range(len(targets)), num_rows, replace=False)
+        
+        # get random features from min == 1 to max_features.
+        num_features = np.random.randint(1, int(data.shape[1]*self.max_features))
+        feature_index = np.random.choice(range(data.shape[1]), num_features, replace=False)
+        return data[row_index][:,feature_index] , targets[row_index], feature_index
+    
+    def fit(self, data, targets):
+        self.num_labels = len(set(targets))
+        self.forest = self.plant_forest()
+        self.trees_features = []
+        for tree in self.forest:
+            features_subset, targets_subset, features_index = self.random_slice(data, targets)
+            self.trees_features.append(features_index)
+            tree.fit(features_subset, targets_subset)
+        
+    def predict(self, features):
+        preds = np.zeros(shape=(features.shape[0], self.num_labels))
+        for tree, feature_mask in zip(self.forest, self.trees_features):
+            preds+=tree.predict_proba(features[:,feature_mask])
+        return np.argmax(preds / len(self.forest), axis=1)
